50876d5b161c670cb8140acefaff206b29500513
M Gimenez-Peterson <mikkergp@modquiz.com>
2018-09-19 08:29:47 -0700

    Write Notes to disk as markdown

--- controllers/sync.js
+++ controllers/sync.js
@@ -13,11 +13,39 @@ exports.index = (req, res) => {
 };
 
 exports.sync = (req, res) => {
+  var fs = require('fs');
+
+  if (!fs.existsSync("backup")){
+      fs.mkdirSync("backup");
+  }
+  if (!fs.existsSync("backup/archived")){
+      fs.mkdirSync("backup/archived");
+  }
+
+
   Note.find(function (err, notes) {
     notes.forEach(function(note) {
-      var fs = require('fs');
+      console.log(note);
 
-      fs.writeFile("backup/test_file", "Hey there!", function(err) {
+      var content = ""
+      content += "---\n"
+      content += `id: ${note._id}\n`;
+      content += `tags: ${note.tags.join(",")}\n`;
+      content += `archived: ${note.archived}\n`;
+      content += `title: ${note.text}\n`;
+      content += `type: ${note.type}\n`;
+      content += `domain: ${note.domain}\n`;
+      content += `createdAt: ${note.createdAt}\n`;
+      content += `updatedAt: ${note.updatedAt}\n`;
+      content += "---\n"
+      content += note.description
+
+      if (note.archived) {
+        fileName = `backup/archived/${note.text.replace("/", "_")}.md`;
+      } else {
+        fileName = `backup/${note.text.replace("/", "_")}.md`;
+      }
+      fs.writeFile(fileName, content, function(err) {
           if(err) {
               return console.log(err);
           }
